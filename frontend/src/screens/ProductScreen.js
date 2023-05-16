f1f98d3c4cf279c42ec997f6594f8e2f1b0a24f4
Lawrence Caulker <locaulker@gmail.com>
2023-05-16 06:48:28 -0400

    resume L10 @ 3:45

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
-  return <div>ProductScreen</div>
+const ProductScreen = ({ match }) => {
+  const params = useParams()
+
+  const product = products.find(p => p._id === params.id)
+  console.log(product)
+
+  return (
+    <>
+      <Link className='btn btn-dark my-3' to='/'>
+        Go back
+      </Link>
+    </>
+  )
 }
 
 export default ProductScreen
