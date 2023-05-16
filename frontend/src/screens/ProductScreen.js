e3626dc1325f1bfefaca7a979e748e45e290431c
Lawrence Caulker <locaulker@gmail.com>
2023-05-16 07:03:31 -0400

    resume @ 6:46

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -15,6 +15,25 @@ const ProductScreen = ({ match }) => {
       <Link className='btn btn-dark my-3' to='/'>
         Go back
       </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
     </>
   )
 }
