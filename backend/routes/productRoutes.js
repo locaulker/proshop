235ad5295dc7b3d0b43e4a18a26b4871fb3fe134
Lawrence Caulker <locaulker@gmail.com>
2023-05-20 19:41:28 -0400

    end custom error message

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,13 @@
-import express from "express"
-import asyncHandler from "express-async-handler"
+import express from 'express'
+import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from "../models/productModel.js"
+import Product from '../models/productModel.js'
 
 // @desc      Fetch ALL products
 // @route     GET /api/v1/products
 // @access    Public
 router.get(
-  "/",
+  '/',
   asyncHandler(async (req, res) => {
     const products = await Product.find({})
 
@@ -19,14 +19,15 @@ router.get(
 // @route     GET /api/v1/products/:id
 // @access    Public
 router.get(
-  "/:id",
+  '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
     if (product) {
       res.json(product)
     } else {
-      res.status(404).json({ message: "Product not found" })
+      res.status(404)
+      throw new Error('Product not found')
     }
   })
 )
