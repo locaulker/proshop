cb8c22b227487a2a662cd288dbda35872dbace4f
Lawrence Caulker <locaulker@gmail.com>
2023-05-20 02:37:39 -0400

    completed seeder script and imported data into compass

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,17 +1,22 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
-const reviewSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  rating: { type: Number, required: true },
-  comment: { type: String, required: true }
-})
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true }
+  },
+  {
+    timeStamps: true
+  }
+)
 
 const productSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User'
+      ref: "User"
     },
     name: {
       type: String,
@@ -22,10 +27,6 @@ const productSchema = mongoose.Schema(
       required: true
     },
     brand: {
-      type: String,
-      required: true
-    },
-    category: {
       type: String,
       required: true
     },
@@ -64,6 +65,6 @@ const productSchema = mongoose.Schema(
   }
 )
 
-const Product = mongoose.model('Product', productSchema)
+const Product = mongoose.model("Product", productSchema)
 
 export default Product
